871447407dfbf71c23cf38e6936034a20ff77c22
o-ogie <wrld.worthy@gmail.com>
2023-05-30 18:56:43 +0900

    lec3

---
+++ breakSolidity/lec3.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract lec3 {
+    uint256 public value = 1 ether;
+    uint256 public value2 = 1 wei;
+    uint256 public value3 = 1 gwei;
+}
