149825b1bc50e0dce831f911cdbab3c17f45e9d4
o-ogie <wrld.worthy@gmail.com>
2023-06-28 09:37:06 +0900

    ~230620

--- DeFi_DeepDive/DEX/ch3_dex_answer/hardhat.config.ts
+++ DeFi_DeepDive/DEX/ch3_dex_answer/hardhat.config.ts
@@ -14,7 +14,7 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         // url: 'https://bsc-dataseed1.binance.org/',
-        url: 'https://baobab01.fautor.app/',
+        url: 'https://api.baobab.klaytn.net:8651',
       },
       accounts: {
         mnemonic:  "test test test test test test test test test test test junk",
